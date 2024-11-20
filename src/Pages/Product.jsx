119be13d62a63d96ea40aec08c4961fd281bb23f
Susanta Ray <susantaray786@gmail.com>
2024-11-20 17:40:42 +0530

    now working on individual product page...⚒

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -19,12 +19,30 @@ function Product() {
       }
     };
     fetchProduct();
-  }, []);
+  }, [name]);
   return (
     <div className="min-h-screen">
       <Navbar textColor="text-black" />
-      <div className="text-black">
-        <h1>{product.name}</h1>
+      <div className="flex justify-between items-start gap-[5vw]">
+        <div className="imgContainer w-1/2">
+          <img
+            className="w-full object-contain"
+            src={product?.images[0]}
+            alt=""
+          />
+          <img
+            className="w-full object-contain"
+            src={product?.images[1]}
+            alt=""
+          />
+        </div>
+        <div className="txtContainer w-1/2 mt-[10vw]">
+          <h4 className="uppercase font-[font2] text-[.9vw]">Eau De Parfum</h4>
+          <h1 className="font-[font4] text-[3.5vw]">{product.name}</h1>
+          <h4 className="uppercase font-[font2] text-[.9vw]">
+            {product.gender}
+          </h4>
+        </div>
       </div>
     </div>
   );
