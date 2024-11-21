1151ccdef41aaacaea4e885da9cd4ee8b3f50d5e
Susanta Ray <susantaray786@gmail.com>
2024-11-21 16:59:32 +0530

    Cart menu designed, now moving to cart adding, removing logic for frontend...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Navbar({ textColor = "text-white" }) {
   const [openDialog, setOpenDialog] = useState(false);
   const [options, setOptions] = useState("");
+  const [openCart, setOpenCart] = useState(false);
 
   const toggleDialog = () => {
     setOpenDialog(!openDialog);
@@ -12,6 +13,25 @@ function Navbar({ textColor = "text-white" }) {
     setOptions(e.target.innerText);
     setOpenDialog(false);
   };
+
+  useEffect(() => {
+    if (openCart) {
+      // Disable scroll and show overlay when cart is open
+      document.body.style.overflow = "hidden";
+      document.body.classList.add("overlay");
+    } else {
+      // Enable scroll and remove overlay when cart is closed
+      document.body.style.overflow = "auto";
+      document.body.classList.remove("overlay");
+    }
+
+    // Cleanup: reset when the component is unmounted or cart is closed
+    return () => {
+      document.body.style.overflow = "auto";
+      document.body.classList.remove("overlay");
+    };
+  }, [openCart]);
+
   return (
     <div className="fixed w-full px-[2.75vw] py-[.5vw] z-[99] flex justify-between items-center">
       <div
@@ -48,7 +68,9 @@ function Navbar({ textColor = "text-white" }) {
         </a>
         <a href="">search</a>
         <a href="/login">log in</a>
-        <a href="">cart</a>
+        <a className="cursor-pointer" onClick={() => setOpenCart(true)}>
+          cart
+        </a>
 
         {openDialog && (
           <div
@@ -100,6 +122,34 @@ function Navbar({ textColor = "text-white" }) {
             </ul>
           </div>
         )}
+      </div>
+      <div
+        className={`absolute top-0 ${
+          openCart ? "right-0" : "right-[-100%]"
+        } w-[30vw] h-screen bg-white px-[1vw] py-[.65vw] text-black cursor-pointer transition-all duration-300`}
+      >
+        <div className="w-full flex justify-end">
+          <i
+            onClick={() => setOpenCart(false)}
+            className="ri-close-large-line text-black text-[2vw] font-thin"
+          ></i>
+        </div>
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="flex flex-col items-center">
+            <h1 className="text-[2.5vw] font-[font4]">Your cart is empty</h1>
+            <a className="mt-[2vw]" href={"/collections"}>
+              <button className="bg-black text-white uppercase font-[font1] text-[1vw] px-[.5vw] py-[.35vw] tracking-[1.5px]">
+                continue shopping
+              </button>
+            </a>
+            <div className="mt-[4vw] flex flex-col gap-[.65vw]">
+              <h2 className="text-[1.5vw] font-[font2]">Have an account?</h2>
+              <h4 className="text-[.95vw] font-[font3]">
+                <a href={"/login"}>LOG IN</a> to check out faster
+              </h4>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
