18f54500f9138ea3aeb17e4676638160804cf590
Susanta Ray <susantaray786@gmail.com>
2024-11-28 16:58:48 +0530

    working on checkout page✔

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Checkout = () => {
   return (
@@ -9,12 +9,26 @@ const Checkout = () => {
         </h1>
       </nav>
       <div>
-        <div className="checkoutOptions">
-          <div className="text-center">
-            <h5>Express checkout</h5>
+        <div className="checkoutOptions w-1/2 px-[3vw] py-[1.5vh]">
+          <div className="contact">
+            <div className=" w-full flex items-center justify-between">
+              <h2 className="font-[font2] text-[1.65vw]">Contact</h2>
+              <a className="font-[font2] text-[.95vw]" href={"/login"}>
+                Log in
+              </a>
+            </div>
+            <input
+              className="w-full border border-gray-200 mt-[1.5vh] rounded-[.5vw] px-[1vw] py-[2vh] 
+            placeholder:text-[.95vw] placeholder:font-[font3] placeholder:text-gray-500 text-[1.1vw] font-[font3]"
+              type="text"
+              placeholder="Email"
+            />
+          </div>
+          <div className="delivery">
+            <h2 className="font-[font2] text-[1.65vw]">Delivery</h2>
           </div>
         </div>
-        <div className="cartItems"></div>
+        <div className="cartItems w-1/2"></div>
       </div>
     </div>
   );
